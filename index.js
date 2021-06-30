4ede3ff067147b17ed129ec189e655cff2dec891
Kristen Prescott <prescottdarling@gmail.com>
2021-06-29 22:18:30 -0400

    Make better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import postRoutes from "./routes/posts.js";
 app.use(express.static(path.join(__dirname, "build")));
 const PORT = process.env.PORT || 8080;
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
